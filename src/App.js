3754709f85f146a6ca374ac9b780bb87302b0fed
marysonthego <marysonthego@gmail.com>
2023-01-20 14:38:33 -0700

    working shapes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import { createRef, forwardRef, useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
+import {MyCanvas } from './MyCanvas';
 
 function App() {
   const [total, setTotal] = useState(0);
   const [shapes, setShapes] = useState([]);
   const ref = useRef(null);
+  //const offRef = useRef(null);
+  const reload = () => window.location.reload(true);
 
   const handleInput = (e) => {
     e.preventDefault();
-    createShapes();
+    if(total > 0 && total < 501){
+      createShapes();
+      //MyOffscreenCanvas({width:"800", height:"800", shapes:{shapes}, ref:{offRef}});
+    }
   };
 
   const createShapes = () => {
-    //console.log(`createShapes Total=`, total);
+    console.log(`createShapes Total=`, total);
 
     for (let i = 0; i < total; i++) {
       let id = i;
+      let shapeName = "Mary" + i;
       let x = getRandomIntInclusive(0, 800);
       //console.log(`createShapes random x =`, x);
 
       let y = getRandomIntInclusive(0, 800);
       //console.log(`random y =`, y);
 
-      let size = getRandomIntInclusive(5, 15);
+      let size = getRandomFloat(5.0, 15.0);
       //console.log(`random size =`, size);
 
       let type = getRandomIntInclusive(0, 2);
       //console.log(`random type =`, type);
+      let typeName;
+      if (type === 0) typeName="Square";
+      if (type === 1) typeName="Circle";
+      if (type === 2) typeName="Triangle";
 
       let r = getRandomIntInclusive(0, 255).toString(16).toUpperCase();
       if (r.length === 1) {
@@ -50,8 +61,8 @@ function App() {
       //let myColor = "".concat(r, g, b);
       //console.log(`mycolor=`, myColor);
 
-      const shapeObj = { id, x, y, size, type, myColor };
-      //console.log(`createShapes shapeObj=`, JSON.stringify(shapeObj));
+      const shapeObj = { shapeName, id, x, y, size, type, typeName, myColor };
+      console.log(`createShapes shapeObj=`, JSON.stringify(shapeObj));
       setShapes(
         shapes => [...shapes, shapeObj],
         );
@@ -64,51 +75,9 @@ function App() {
     }
   };
 
-  const MyCanvas = forwardRef(function MyCanvas({width, height, shapes}, ref) {
-
-    useEffect(() => {
-      if (ref.current) {
-        const ctx = ref.current.getContext("2d");
-        shapes.forEach(el => {
-          switch (el.type) {
-            case '0':
-              console.log('case 0');
-              break;
-            case 1:
-              console.log('case 1');
-              break;
-            case 2:
-              console.log('case 2');
-              break;
-            default:
-            break;
-          }
-        });
-        ctx.strokeRect(200, 200, 40, 50);
-      };
-    }, [ref]);
-
-    return (
-      <>
-      <canvas
-        ref={ref}
-        width="600"
-        height="600"
-        style={{ border: "2px solid black" }}
-      />
-      </>
-    );
-  });
-
-  const Rectangle = () => {
-    const ref = useRef(HTMLCanvasElement);
-    useEffect(() => {
-      if (ref.current) {
-        const ctx = ref.current.getContext("2d");
-        ctx.strokeRect(200, 200, 40, 50);
-      }
-    }, [ref]);
-  };
+  function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
   return (
     <div className="App">
@@ -124,24 +93,17 @@ function App() {
             onChange={e => setTotal(e.target.value)}
           />
           <span className="validity"></span>
-          <>
             <button onClick={(e) => {
               handleInput(e); }} >
               Submit
             </button>
-          </>
+            {"  "}
+            <button onClick={reload}>
+              Reset
+            </button>
           </span>
       </header>
       <div>
-      <ul>
-        {
-        shapes.map(({id, x, y, size, type, myColor}) => (
-
-          <li key={id} color={myColor}>x={x} y={y} size={size} type={type} color={myColor}</li>
-
-        ))
-        }
-      </ul>
       </div>
       <div>
       <MyCanvas width="600" height="600" shapes={shapes} ref={ref}/>
--- src/MyCanvas.js
+++ src/MyCanvas.js
@@ -1,14 +1,126 @@
-import { createRef, forwardRef, useEffect, useRef, useState } from "react";
+import { forwardRef, useEffect } from "react";
 
 const MyCanvas = forwardRef(function MyCanvas({width, height, shapes}, ref) {
+  console.table(shapes);
 
   useEffect(() => {
-    if (ref.current) {
-      const ctx = ref.current.getContext("2d");
-      ctx.strokeRect(200, 200, 40, 50);
-    }
-  });
+   if (ref.current) {
+    const ctx = ref.current.getContext("2d");
+    let side = 0;
+    shapes.forEach(el => {
+      ctx.fillStyle = el.myColor;
+      ctx.strokeStyle = el.myColor;
+      switch (el.type) {
+        case 0: //Square
+          //console.log('case 0');
+            side = el.size/Math.SQRT2;
+            ctx.fillRect(el.x, el.y, side, side);
+          break;
+        case 1:  //Circle
+          //console.log('case 1');
+          ctx.beginPath();
+          ctx.arc(el.x, el.y, el.size, 0, Math.PI * 2, true);
+          ctx.fill();
 
+          break;
+        case 2:  //Triangle
+          //console.log('case 2');
+          side = el.size * Math.sqrt(3);
+          let R = (side * .5) / Math.cos(Math.PI/6);
+          //let height = Math.sqrt(3) / 2 * side;
+          //console.log(`R=`, R, `size=`, el.size, `side=`, side);
+          let triangle = {
+            x1: el.x + R,
+            y1: el.y,
+            x2: el.x + R * Math.cos(2 * Math.PI/3),
+            y2: el.y + R * Math.sin(2 * Math.PI/3),
+            x3: el.x + R * Math.cos(4 * Math.PI/3),
+            y3: el.y + R * Math.sin(4 * Math.PI/3)
+          };
+          //console.log(`el.x=`, el.x, `el.y=`, el.y);
+          //console.log(`x1,y1=${triangle.x1} ${triangle.y1} x2,y2=${triangle.x2} ${triangle.y2} x3,y3=${triangle.x3} ${triangle.y3}` );
+          ctx.beginPath();
+          ctx.moveTo(triangle.x1, triangle.y1);
+          ctx.lineTo(triangle.x2, triangle.y2);
+          ctx.lineTo(triangle.x3, triangle.y3);
+          ctx.lineTo(triangle.x1, triangle.y1);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.fill();
+          break;
+        default:
+        break;
+      }
+    });
+  };
+},[ref, shapes]);
+
+  return (
+    <>
+    <canvas
+      ref={ref}
+      width="800"
+      height="800"
+      style={{ border: "2px solid black" }}
+    />
+    </>
+  );
+});
+
+const MyOffscreenCanvas = forwardRef(function MyOffscreenCanvas({width, height, shapes}, ref) {
+  //console.table(shapes);
+
+  useEffect(() => {
+   if (ref.current) {
+    const ctx = ref.current.getContext("2d");
+    let side = 0;
+    shapes.forEach(el => {
+      ctx.fillStyle = el.myColor;
+      ctx.strokeStyle = el.myColor;
+      switch (el.type) {
+        case 0: //Square
+          //console.log('case 0');
+            side = el.size/Math.SQRT2;
+            ctx.fillRect(el.x, el.y, side, side);
+          break;
+        case 1:  //Circle
+          //console.log('case 1');
+          ctx.beginPath();
+          ctx.arc(el.x, el.y, el.size, 0, Math.PI * 2, true);
+          ctx.fill();
+
+          break;
+        case 2:  //Triangle
+          //console.log('case 2');
+          side = el.size * Math.sqrt(3);
+          let R = (side * .5) / Math.cos(Math.PI/6);
+          //let height = Math.sqrt(3) / 2 * side;
+          //console.log(`R=`, R, `size=`, el.size, `side=`, side);
+          let triangle = {
+            x1: el.x + R,
+            y1: el.y,
+            x2: el.x + R * Math.cos(2 * Math.PI/3),
+            y2: el.y + R * Math.sin(2 * Math.PI/3),
+            x3: el.x + R * Math.cos(4 * Math.PI/3),
+            y3: el.y + R * Math.sin(4 * Math.PI/3)
+          };
+          //console.log(`el.x=`, el.x, `el.y=`, el.y);
+          //console.log(`x1,y1=${triangle.x1} ${triangle.y1} x2,y2=${triangle.x2} ${triangle.y2} x3,y3=${triangle.x3} ${triangle.y3}` );
+          ctx.beginPath();
+          ctx.moveTo(triangle.x1, triangle.y1);
+          ctx.lineTo(triangle.x2, triangle.y2);
+          ctx.lineTo(triangle.x3, triangle.y3);
+          ctx.lineTo(triangle.x1, triangle.y1);
+          ctx.closePath();
+          ctx.stroke();
+          ctx.fill();
+          break;
+        default:
+        break;
+      }
+    });
+  };
+},[ref, shapes]);
 
   return (
     <>
@@ -22,14 +134,6 @@ const MyCanvas = forwardRef(function MyCanvas({width, height, shapes}, ref) {
   );
 });
 
-const Rectangle = () => {
-  const ref = useRef(HTMLCanvasElement);
-  useEffect(() => {
-    if (ref.current) {
-      const ctx = ref.current.getContext("2d");
-      ctx.strokeRect(200, 200, 40, 50);
-    }
-  }, [ref]);
-};
 
-export {MyCanvas}
+
+export {MyCanvas, MyOffscreenCanvas}
